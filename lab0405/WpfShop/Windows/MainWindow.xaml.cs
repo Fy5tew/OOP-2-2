d3df43809dee878079f707eee0001f4031311f59
Nikita <nikturchinovich@mail.ru>
2024-04-17 18:17:54 +0300

    lab04-lab05 is done

---
+++ lab0405/WpfShop/Windows/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfShop {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private JsonStorage<BindingList<ProductModel>> _storage = new JsonStorage<BindingList<ProductModel>>(@"..\..\Storage\data.json");
+        private BindingList<ProductModel> _productsDataList;
+
+        public MainWindow() {
+            InitializeComponent();
+            LoadCursor();
+            LoadData();
+
+            SetLocalization(LocalizationCode.RU);
+            SetWallpapers();
+
+            ProductsDataGrid.ItemsSource = _productsDataList;
+
+            _productsDataList.ListChanged += OnDataListChanged;
+        }
+
+        private void SetLocalization(LocalizationCode localizationCode) {
+            this.Resources.MergedDictionaries.Add(LocalizationResources.GetResource(localizationCode));
+        }
+
+        private void SetWallpapers() {
+            this.Resources.Add("WallpapersSrc", new BitmapImage(new Uri(@"..\..\Assets\spaceWallpapers.jpg", UriKind.Relative)));
+        }
+
+        private void LoadCursor() {
+            string cursorPath = @"..\..\Assets\cursor.ani";
+            using (var fs = new FileStream(cursorPath, FileMode.Open)) {
+                var cursor = new Cursor(fs);
+                this.Cursor = cursor;
+            }
+        }
+
+        private void LoadData() {
+            try {
+                _productsDataList = _storage.Load() ?? new BindingList<ProductModel>();
+                ProductsCountLabel.Text = _productsDataList.Count.ToString();
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Ошибка при загрузке данных {ex}");
+                Environment.Exit(0);
+            }
+        }
+
+        private void SaveData() {
+            try {
+                _storage.Save(_productsDataList);
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Ошибка при сохранении данных: {ex}");
+                Environment.Exit(0);
+            }
+        }
+
+        private void OnDataListChanged(object sender, ListChangedEventArgs e) {
+            ProductsCountLabel.Text = _productsDataList.Count.ToString();
+            SaveData();
+        }
+
+        private void AddProductButton_Click(object sender, RoutedEventArgs e) {
+            AddProductWindow addProductWindow = new AddProductWindow(_productsDataList);
+            addProductWindow.Resources.MergedDictionaries.Add(this.Resources);
+            addProductWindow.Cursor = this.Cursor;
+            addProductWindow.ShowDialog();
+        }
+
+        private void EditProductsButton_Click(object sender, RoutedEventArgs e) {
+            EditProductsWindow editProductsWindow = new EditProductsWindow(_productsDataList);
+            editProductsWindow.Resources.MergedDictionaries.Add(this.Resources);
+            editProductsWindow.Cursor = this.Cursor;
+            editProductsWindow.ShowDialog();
+        }
+
+        private void ShowProductsButton_Click(object sender, RoutedEventArgs e) {
+            ShowProductsWindow showProductsWindow = new ShowProductsWindow(_productsDataList);
+            showProductsWindow.Resources.MergedDictionaries.Add(this.Resources);
+            showProductsWindow.Cursor = this.Cursor;
+            showProductsWindow.ShowDialog();
+        }
+
+        private void LocalizationRUButton_Click(object sender, RoutedEventArgs e) {
+            SetLocalization(LocalizationCode.RU);
+        }
+
+        private void LocalizationENButton_Click(object sender, RoutedEventArgs e) {
+            SetLocalization(LocalizationCode.EN);
+        }
+    }
+}
---
+++ lab0405/WpfShop/Windows/ShowProductsWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfShop {
+    /// <summary>
+    /// Логика взаимодействия для ShowProductsWindow.xaml
+    /// </summary>
+    public partial class ShowProductsWindow : Window {
+        private BindingList<ProductModel> _productsOriginal;
+        private List<ProductModel> _productsCurrent;
+        public ShowProductsWindow(BindingList<ProductModel> data) {
+            InitializeComponent();
+
+            _productsOriginal = data;
+
+            Reset();
+        }
+
+        private void Set(List<ProductModel> data) {
+            _productsCurrent = data;
+            ProductsDataGrid.ItemsSource = _productsCurrent;
+        }
+
+        private void Reset() {
+            Set(_productsOriginal.ToList());
+        }
+
+        private void OnlyDecimal_PreviewTextInput(object sender, TextCompositionEventArgs e) {
+            if (!Regex.IsMatch(e.Text, @"^\d$") && (e.Text != ".")) {
+                e.Handled = true;
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e) {
+            Reset();
+        }
+
+        private void SortingIDAscButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderBy(p => p.ID).ToList());
+        }
+
+        private void SortingIDDescButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderByDescending(p => p.ID).ToList());
+        }
+
+        private void SortingRatingAscButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderBy(p => p.Rating).ToList());
+        }
+
+        private void SortingRatingDescButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderByDescending(p => p.Rating).ToList());
+        }
+
+        private void SortingPriceAscButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderBy(p => p.Price).ToList());
+        }
+
+        private void SortingPriceDescButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderByDescending(p => p.Price).ToList());
+        }
+
+        private void SortingQuantityAscButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderBy(p => p.Quantity).ToList());
+        }
+
+        private void SortingQuantityDescButton_Click(object sender, RoutedEventArgs e) {
+            Set(_productsCurrent.OrderByDescending(p => p.Quantity).ToList());
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            Reset();
+            Set(_productsCurrent.Where(p => p.Title.ToLower().Contains(SearchTextBox.Text.ToLower())).ToList());
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e) {
+            try {
+                decimal from, to;
+                if (FilteringPriceFromTextBox.Text == string.Empty) {
+                    from = 0;
+                }
+                else {
+                    from = Convert.ToDecimal(FilteringPriceFromTextBox.Text);
+                }
+                if (FilteringPriceToTextBox.Text == string.Empty) {
+                    to = decimal.MaxValue;
+                }
+                else {
+                    to = Convert.ToDecimal(FilteringPriceToTextBox.Text);
+                }
+                Reset();
+                Set(_productsCurrent.Where(p => p.Price >= from && p.Price <= to).ToList());
+            }
+            catch {
+                MessageBox.Show("Неверный диапазон цен");
+            }
+        }
+    }
+}
